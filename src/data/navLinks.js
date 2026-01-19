ee7b99df781e764a246e94d272a4386aa26c704f
Chris Smart <chrissmart920@gmail.com>
2026-01-19 18:03:46 +0000

    services page complete

--- src/data/navLinks.js
+++ src/data/navLinks.js
@@ -1,6 +1,6 @@
 export const navLinks = [
   { href: "/", label: "Home" },
-  { href: "/", label: "Services" },
+  { href: "/services", label: "Services" },
   { href: "/", label: "Projects" },
   { href: "/", label: "About" },
   { href: "/", label: "Contact" },
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,13 +10,21 @@ export default {
         light: "#FAF9f7", // Off White
         dark: "#1C1A1D", // Ink
       },
+      // backgroundImage: {
+      //   "gradient-primary": "linear-gradient(135deg, #1C1A1D, #4A3F55)",
+      //   "gradient-warm": "linear-gradient(135deg, #4A3F55, #8C6A4A)",
+      //   "gradient-soft": "linear-gradient(180deg, #FAF9F7, #EFEDEA)",
+      //   "gradient-dark": "linear-gradient(180deg, #2A2A2A 0%, #1C1A1D 100%)",
+      //   "gradient-footer": "linear-gradient(180deg, #1C1A1F 0%, #141018 100%)",
+      // },
       backgroundImage: {
-        "gradient-primary": "linear-gradient(135deg, #1C1A1D, #4A3F55)",
-        "gradient-warm": "linear-gradient(135deg, #4A3F55, #8C6A4A)",
-        "gradient-soft": "linear-gradient(180deg, #FAF9F7, #EFEDEA)",
-        "gradient-dark": "linear-gradient(180deg, #1C1A1D 0%, #2A2A2A 100%)",
-        "gradient-footer": "linear-gradient(180deg, #262626 0%, #392F42 100%)",
+        "gradient-primary": "linear-gradient(135deg, #1C1A1D 0%, #43384C 100%)",
+        "gradient-warm": "linear-gradient(135deg, #43384C 0%, #8C6A4A 100%)",
+        "gradient-soft": "linear-gradient(180deg, #FAF9F7 0%, #F0EEEB 100%)",
+        "gradient-dark": "linear-gradient(180deg, #2A2A2A  0%, #1C1A1D 100%)",
+        "gradient-footer": "linear-gradient(180deg, #18171A 0%, #121115 100%)",
       },
+
       fontFamily: {
         sans: ["Lora", "serif"],
         heading: ["Poppins", "sans-serif"],
