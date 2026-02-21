e0eb3fd5aceacf8cd3e48db58871d0448993dd1f
Chris Smart <chrissmart920@gmail.com>
2026-02-21 08:14:08 +0000

    slight content changes

--- src/data/navLinks.js
+++ src/data/navLinks.js
@@ -1,7 +1,7 @@
 export const navLinks = [
   { href: "/", label: "Home" },
-  { href: "/services", label: "Services" },
-  { href: "/projects", label: "Projects" },
-  { href: "/about", label: "About" },
-  { href: "/contact", label: "Contact" },
+  { href: "/services/", label: "Services" },
+  { href: "/projects/", label: "Projects" },
+  { href: "/about/", label: "About" },
+  { href: "/contact/", label: "Contact" },
 ];
