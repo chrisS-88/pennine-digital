696c79e89d8a56de855f4ebc6bcbf574e62b2a5f
Chris Smart <chrissmart920@gmail.com>
2026-05-09 07:40:19 +0100

    broken logo paths fixed

--- src/data/navLinks.js
+++ src/data/navLinks.js
@@ -1,7 +1,7 @@
 export const navLinks = [
   { href: "/", label: "Home" },
-  { href: "/services", label: "Services" },
-  { href: "/projects", label: "Work" },
-  { href: "/about", label: "About" },
-  { href: "/contact", label: "Contact" },
+  { href: "/services/", label: "Services" },
+  { href: "/projects/", label: "Work" },
+  { href: "/about/", label: "About" },
+  { href: "/contact/", label: "Contact" },
 ];
